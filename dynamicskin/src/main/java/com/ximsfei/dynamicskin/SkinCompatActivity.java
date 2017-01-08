9dd10218a3f08a8018c621ec0341b41ad42a1c63
pengfengwang <550616352@qq.com>
2017-01-09 00:43:13 +0800

    SkinCompatActivity
    Signed-off-by: pengfengwang <550616352@qq.com>

---
+++ dynamicskin/src/main/java/com/ximsfei/dynamicskin/SkinCompatActivity.java
@@ -0,0 +1,17 @@
+package com.ximsfei.dynamicskin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ximsfei on 17-1-8.
+ */
+
+public class SkinCompatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
